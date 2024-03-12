4d3e5f0b9146548e125dbf222df9026c04c8b255
maceireggre <157102399+maceireggre@users.noreply.github.com>
2024-03-12 23:54:55 +0100

    Update gomoku_affichage.py

--- gomoku_affichage.py
+++ gomoku_affichage.py
@@ -1,16 +1,11 @@
 from tkinter import *
+import math
 
-Fenetre = Tk()
-
-area_draw = Canvas(Fenetre,width=225,height=225,bg="white", bd=0)
-area_draw.pack()
-#horizontal
-for i in range(16):
-
-    area_draw.create_line(0,0+15,225,0+15, fill="black",width=2)
-
-area_draw.create_line(10,225,10,0, fill="black",width=2)
-Fenetre.mainloop()
+FACT=30
+LINES=16
+WIDTH=FACT*(LINES-1)
+OFFSET=10
+RADIUS=10
 
 class App(Tk):
     def __init__(self):
@@ -19,17 +14,7 @@ class App(Tk):
         self.label = Label(self)
         self.label.pack()
 
-        self.bind("-", lambda e: self.dezoom())
-        self.bind("<BackSpace>", lambda e: self.retour_vue_precedente())
         self.bind('<Button-1>', lambda e: self.click(e.x, e.y))
-        self.bind('<ButtonRelease-1>', lambda e: self.click_relache(e.x, e.y))
-
-        # gestion des coordonnées pour l'image
-        # au depart on trace l'image de cote [-2, 2]
-        self.xmin, self.ymin = -2, -2
-        self.xmax, self.ymax = 2, 2
-
-        # vous pouvez ajouter des attributes pour gérer le zoom dans un rectangle
 
     def affiche_image(self, pil_img):
         """affiche l'image donnée dans tkinter"""
@@ -37,14 +22,20 @@ class App(Tk):
         self.label.configure(image=tk_img)
         self.label.image = tk_img
 
-    def dezoom(self):
-        print("-") # a modifier
-
     def click(self, x, y):
-        print("click", x, y) # a modifier
+        xr=OFFSET+math.floor((x+FACT/2-OFFSET)/FACT)*FACT
+        yr=OFFSET+math.floor((y+FACT/2-OFFSET)/FACT)*FACT
+        print("click", x, y,"->",xr,yr)
+        area_draw.create_oval(xr-RADIUS,yr-RADIUS,xr+RADIUS,yr+RADIUS,fill="black", outline="green")
 
-    def click_relache(self, x, y):
-        print("relache", x, y) # a modifier
+Fenetre = App()
 
-    def retour_vue_precedente(self):
-        print("retour") # a modifier
+area_draw = Canvas(Fenetre,width=WIDTH+2*OFFSET,height=WIDTH+2*OFFSET,bg="white", bd=0)
+area_draw.pack()
+#horizontal
+for i in range(LINES):
+    area_draw.create_line(OFFSET,i*FACT+OFFSET,WIDTH+OFFSET,i*FACT+OFFSET, fill="black",width=2)
+for i in range(LINES):
+    area_draw.create_line(i*FACT+OFFSET,OFFSET,i*FACT+OFFSET,WIDTH+OFFSET, fill="black",width=2)
+
+Fenetre.mainloop()
